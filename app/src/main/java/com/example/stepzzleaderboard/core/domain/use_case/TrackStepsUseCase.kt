6558db1b3fad985bfd639fd56c82e292aa59a3a8
Omare Faruk <ofaruk0169@gmail.com>
2025-07-01 22:32:51 +0100

    added track steps use case

--- app/src/main/java/com/example/stepzzleaderboard/core/domain/use_case/TrackStepsUseCase.kt
+++ app/src/main/java/com/example/stepzzleaderboard/core/domain/use_case/TrackStepsUseCase.kt
@@ -1,5 +1,6 @@
 package com.example.stepzzleaderboard.core.domain.use_case
 
+import com.example.stepzzleaderboard.core.domain.model.StepsEntity
 import com.example.stepzzleaderboard.core.domain.repository.StepRepository
 
 //this will record how many steps were walked on a day.
@@ -7,4 +8,7 @@ import com.example.stepzzleaderboard.core.domain.repository.StepRepository
 class TrackStepsUseCase(
     private val stepRepository: StepRepository
 ) {
+    suspend operator fun invoke(userId: Int, stepCount: Int): Result<StepsEntity> {
+        return stepRepository.recordSteps(userId, stepCount)
+    }
 }
