2154fa46a0e4331e8580c23779383bbbfea69462
Omare Faruk <ofaruk0169@gmail.com>
2026-01-13 21:29:12 +0000

    getters and setters

--- app/src/main/java/com/example/stepzzleaderboard/feature_steps/presentation/leaderboard/LeaderboardViewModel.kt
+++ app/src/main/java/com/example/stepzzleaderboard/feature_steps/presentation/leaderboard/LeaderboardViewModel.kt
@@ -1,4 +1,17 @@
 package com.example.stepzzleaderboard.feature_steps.presentation.leaderboard
 
-class LeaderboardViewModel {
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.stepzzleaderboard.feature_steps.domain.use_case.StepUseCases
+import javax.inject.Inject
+
+
+class LeaderboardViewModel @Inject constructor(
+    private val stepUseCases: StepUseCases
+) : ViewModel() {
+
+    private val _state = mutableStateOf(LeaderboardState())
+    val state: State<LeaderboardState> = _state
+
 }
